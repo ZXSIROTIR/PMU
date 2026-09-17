5fc8f527dc477a0d48911f6d7ad2cff582193ea9
ZXSIROTIR <92199008+ZXSIROTIR@users.noreply.github.com>
2026-09-17 09:47:55 +0700

    realization
    added realization of spinner and seekbar

--- Labs/app/src/main/java/com/ip312/labs/MainActivity.kt
+++ Labs/app/src/main/java/com/ip312/labs/MainActivity.kt
@@ -2,11 +2,58 @@ package com.ip312.labs
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Spinner
+import android.widget.SeekBar
+import android.widget.CalendarView
+import android.widget.ArrayAdapter
+import android.widget.TextView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+        val editName = findViewById<EditText>(R.id.editName)
+        val radioGender = findViewById<RadioGroup>(R.id.radioGender)
+        val spinnerCourse = findViewById<Spinner>(R.id.spinnerCourse)
+        val courses = arrayOf(
+            "1 курс",
+            "2 курс",
+            "3 курс",
+            "4 курс"
+        )
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            courses
+        )
+        adapter.setDropDownViewResource(
+            android.R.layout.simple_spinner_dropdown_item
+        )
+        spinnerCourse.adapter = adapter
+
+        val textDifficulty = findViewById<TextView>(R.id.textDifficulty)
+        val seekDifficulty = findViewById<SeekBar>(R.id.seekDifficulty)
+        seekDifficulty.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    textDifficulty.text = "Уровень: ${progress + 1}"
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                }
+            }
+        )
+        val calendarBirth = findViewById<CalendarView>(R.id.calendarBirth)
     }
 }
